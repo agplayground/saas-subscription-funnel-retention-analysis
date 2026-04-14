db8592772f017076ea9122889565187951bb8b8c
agplayground <Augis998@gmail.com>
2026-04-14 18:52:03 +0300

    Update 03_churn_by_month.sql

--- sql/03_churn_by_month.sql
+++ sql/03_churn_by_month.sql
@@ -1,21 +1,21 @@
--- Ranking churn events to use only the latest one in the analysis
+-- Aggregating churn_date as the analysis considers only those accounts that churned and never reactivated
 WITH last_churn AS (
   SELECT 
     account_id, 
-    churn_date,
-    reason_code,
-    ROW_NUMBER() OVER (PARTITION BY account_id ORDER BY churn_date DESC) AS rn
-  FROM `project-7cc9c569-446d-4cb3-a49.saas_subscription_funnel_retention_analysis.churn_events`
+    MAX(churn_date) AS churn_date,
+  FROM churn_events
+  GROUP BY 1
 )
 -- Combining permanently churned accounts data with their churn date for a monthly breakdown
 SELECT 
     DATE_TRUNC(churn_date,MONTH) AS churn_month,
     COUNT(*)
-FROM `project-7cc9c569-446d-4cb3-a49.saas_subscription_funnel_retention_analysis.accounts` a
+FROM accounts a
 LEFT JOIN last_churn lc ON 
   a.account_id = lc.account_id
-  AND lc.rn = 1
 WHERE 
     churn_flag = true
     AND churn_date IS NOT NULL
 GROUP BY 1
+ORDER BY 1
+;
